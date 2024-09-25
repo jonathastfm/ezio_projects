2f13d0997de1ae04ed0b129b681e952ef39ba805
alexlbnt <alexlbnt@hotmail.com>
2024-09-25 14:51:23 -0300

    Refactor code to improve layout and organization

--- com_ezio_osrpjas/lib/pages/Tela_Ficha.dart
+++ com_ezio_osrpjas/lib/pages/Tela_Ficha.dart
@@ -7,18 +7,88 @@ class Tela_Ficha extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            const Text('Ficha'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Voltar'),
+        child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: const [
+                Color.fromARGB(255, 88, 0, 202),
+                Color.fromARGB(255, 47, 0, 109),
+                Color.fromARGB(255, 17, 0, 48)
+              ],
             ),
-          ],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  // Tabela de Atributos do Personagem
+                  Table(
+                    border: TableBorder.all(color: Colors.white),
+                    children: [
+                      _buildTableRow('Força', '18'),
+                      _buildTableRow('Destreza', '16'),
+                      _buildTableRow('Constituição', '15'),
+                      _buildTableRow('Inteligência', '14'),
+                      _buildTableRow('Sabedoria', '12'),
+                      _buildTableRow('Carisma', '10'),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(width: 20), // Espaçamento entre as tabelas
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  // Nova Tabela com Vida, Armadura e Deslocamento
+                  Table(
+                    border: TableBorder.all(color: Colors.white),
+                    children: [
+                      _buildTableRow('Vida', '50'),
+                      _buildTableRow('Armadura', '18'),
+                      _buildTableRow('Deslocamento', '9m'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  TableRow _buildTableRow(String atributo, String valor) {
+    return TableRow(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Text(
+            atributo,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Text(
+            valor,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- com_ezio_osrpjas/lib/pages/header.dart
+++ com_ezio_osrpjas/lib/pages/header.dart
@@ -1,43 +1,110 @@
-import "package:com_ezio_osrpjas/pages/Tela_Ficha.dart";
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:com_ezio_osrpjas/pages/Tela_Ficha.dart';
 
 class Header extends StatelessWidget {
   const Header({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.home),
-                ),
-                Tab(
-                  icon: Icon(Icons.person),
-                ),
-                Tab(
-                  icon: Icon(Icons.settings),
-                ),
-              ],
-            ),
-            title: const Text('Ficha'),
-          ),
-          body: TabBarView(
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(child: Tela_Ficha()),
-              Container(
-                color: Colors.green,
-                child: const Icon(Icons.person),
+              Text(
+                'Área para info básicas',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black, // Cor preta
+                ),
               ),
-              Container(
-                color: Colors.red,
-                child: const Icon(Icons.settings),
+              SizedBox(height: 16), // Espaço entre o título e as informações
+              Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceBetween, // Espaço entre os dois lados
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nome: [?? nome ??]',
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.black), // Cor preta
+                      ),
+                      Text(
+                        'Raça: [?? raça ??]',
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.black), // Cor preta
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Classe: [?? classe ??]',
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.black), // Cor preta
+                      ),
+                      Text(
+                        'Nível: [?? nível ??]',
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.black), // Cor preta
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ],
-          )),
+          ),
+        ),
+        Expanded(
+          child: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              appBar: AppBar(
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.person),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.sports_mma_rounded),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.auto_awesome_rounded),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.shield),
+                    ),
+                  ],
+                ),
+                title: const Text('Ficha'),
+              ),
+              body: const TabBarView(
+                children: [
+                  // Primeira aba com Tela_Ficha()
+                  Tela_Ficha(),
+
+                  // Outras abas com cores de fundo diferentes
+                  ColoredBox(
+                    color: Color.fromARGB(255, 21, 0, 32),
+                  ),
+                  ColoredBox(
+                    color: Color.fromARGB(255, 31, 0, 51),
+                  ),
+                  ColoredBox(
+                    color: Color.fromARGB(255, 31, 0, 51),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
