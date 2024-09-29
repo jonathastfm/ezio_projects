4b17c30d4f8bc02db9e6d7f330768b2be91ae25f
alexlbnt <alexlbnt@hotmail.com>
2024-09-29 14:54:06 -0300

    Modificando a Tela_Ficha

--- com_ezio_osrpjas/lib/pages/Tela_Ficha.dart
+++ com_ezio_osrpjas/lib/pages/Tela_Ficha.dart
@@ -8,60 +8,120 @@ class Tela_Ficha extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           decoration: BoxDecoration(
-            gradient: LinearGradient(
+            gradient: const LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: const [
+              colors: [
                 Color.fromARGB(255, 88, 0, 202),
                 Color.fromARGB(255, 47, 0, 109),
-                Color.fromARGB(255, 17, 0, 48)
+                Color.fromARGB(255, 17, 0, 48),
               ],
             ),
             borderRadius: BorderRadius.circular(10),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 20),
-                  // Tabela de Atributos do Personagem
-                  Table(
-                    border: TableBorder.all(color: Colors.white),
-                    children: [
-                      _buildTableRow('Força', '18'),
-                      _buildTableRow('Destreza', '16'),
-                      _buildTableRow('Constituição', '15'),
-                      _buildTableRow('Inteligência', '14'),
-                      _buildTableRow('Sabedoria', '12'),
-                      _buildTableRow('Carisma', '10'),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(width: 20), // Espaçamento entre as tabelas
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 20),
-                  // Nova Tabela com Vida, Armadura e Deslocamento
-                  Table(
-                    border: TableBorder.all(color: Colors.white),
-                    children: [
-                      _buildTableRow('Vida', '50'),
-                      _buildTableRow('Armadura', '18'),
-                      _buildTableRow('Deslocamento', '9m'),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          _buildTable('Atributos', [
+                            _buildTableRow('Força', '18'),
+                            _buildTableRow('Destreza', '16'),
+                            _buildTableRow('Constituição', '15'),
+                            _buildTableRow('Inteligência', '14'),
+                            _buildTableRow('Sabedoria', '12'),
+                            _buildTableRow('Carisma', '10'),
+                          ]),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: _buildTable('Status', [
+                        _buildTableRow('Vida', '50'),
+                        _buildTableRow('CA', '18'),
+                        _buildTableRow('Deslocamento', '9m'),
+                      ]),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: _buildTable('Perícias', [
+                        _buildTableRow('Atletismo', '8'),
+                        _buildTableRow('Acrobacia', '7'),
+                        _buildTableRow('Furtividade', '6'),
+                        _buildTableRow('Prestidigitação', '5'),
+                        _buildTableRow('Arcanismo', '4'),
+                        _buildTableRow('História', '3'),
+                        _buildTableRow('Investigação', '2'),
+                        _buildTableRow('Natureza', '1'),
+                        _buildTableRow('Religião', '0'),
+                        _buildTableRow('Adestrar Animais', '2'),
+                        _buildTableRow('Intuição', '4'),
+                        _buildTableRow('Medicina', '6'),
+                        _buildTableRow('Percepção', '5'),
+                        _buildTableRow('Sobrevivência', '7'),
+                        _buildTableRow('Atuação', '6'),
+                        _buildTableRow('Enganação', '8'),
+                        _buildTableRow('Intimidação', '9'),
+                        _buildTableRow('Persuasão', '10'),
+                      ]),
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: _buildTable('Teste', [
+                        _buildTableRow('Teste', '1'),
+                        _buildTableRow('Teste', '2'),
+                        _buildTableRow('Teste', '16'),
+                        _buildTableRow('Teste', '3'),
+                        _buildTableRow('Teste', '11'),
+                      ]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTable(String title, List<TableRow> rows) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Table(
+            border: TableBorder.all(
+              color: Colors.white,
+              width: 2,
+            ),
+            children: rows,
+          ),
+        ),
+      ],
     );
   }
 
@@ -69,20 +129,20 @@ class Tela_Ficha extends StatelessWidget {
     return TableRow(
       children: [
         Padding(
-          padding: const EdgeInsets.all(6.0),
+          padding: const EdgeInsets.all(8.0),
           child: Text(
             atributo,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 18,
             ),
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(6.0),
+          padding: const EdgeInsets.all(8.0),
           child: Text(
             valor,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 18,
             ),
